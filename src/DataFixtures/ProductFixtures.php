647501ad494d60206c4d6a0e6e7a454bf204675e
LewisEMO <lemo@normandiewebschool.fr>
2024-10-17 16:50:00 +0200

    Fixtures

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 10; $i++) {
+            $product = new Product();
+            $product->setStatus($faker->randomElement(['draft', 'published']));
+            $product->setCreatedAt($faker->dateTimeThisYear());
+            $product->setUpdatedAt($faker->dateTimeThisMonth());
+
+            $manager->persist($product);
+        }
+
+        $manager->flush();
+    }
+}
