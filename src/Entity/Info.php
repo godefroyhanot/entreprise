b800a5802e5d2bde42d4e376d774c4b344ee5f09
mballuais <mballuais@normandiewebschool.fr>
2024-10-17 14:56:26 +0200

    ClientEntityFIxtures

---
+++ src/Entity/Info.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InfoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InfoRepository::class)]
+class Info
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?bool $isAnonymous = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Infotype $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $info = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updateAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isAnonymous(): ?bool
+    {
+        return $this->isAnonymous;
+    }
+
+    public function setAnonymous(bool $isAnonymous): static
+    {
+        $this->isAnonymous = $isAnonymous;
+
+        return $this;
+    }
+
+    public function getType(): ?Infotype
+    {
+        return $this->type;
+    }
+
+    public function setType(?Infotype $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getInfo(): ?string
+    {
+        return $this->info;
+    }
+
+    public function setInfo(string $info): static
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->updateAt;
+    }
+
+    public function setUpdateAt(\DateTimeImmutable $updateAt): static
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
