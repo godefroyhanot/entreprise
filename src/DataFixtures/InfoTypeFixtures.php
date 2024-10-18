d482d8b280937c0b19d3384d5546035ba2aa41e0
Jin <alexandre.Quilan.delaistre@gmail.com>
2024-10-18 09:51:32 +0200

    fix: conventionnal pbs

--- src/DataFixtures/InfoTypeFixtures.php
+++ src/DataFixtures/InfoTypeFixtures.php
@@ -11,7 +11,9 @@ use Faker\Generator;
 class InfoTypeFixtures extends Fixture
 {
     public const PREFIX = 'infotype#';
-    public const POOL_MAX = 5;
+    public
+    const POOL_MAX = 5;
+    public const POOL_MIN = 0;
 
     private Generator $faker; // Déclaration du type Generator pour Faker
 
@@ -22,13 +24,13 @@ class InfoTypeFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < self::POOL_MAX; $i++) {
+        for ($i = self::POOL_MIN; $i < self::POOL_MAX; $i++) {
             $infoType = new InfoType();
             $infoType->setName('Type ' . $i)
-                     ->setInfo($this->faker->sentence(10)) // Utilition de Faker via $this->faker
-                     ->setCreatedAt($this->faker->dateTimeThisYear()) 
-                     ->setUpdatedAt($this->faker->dateTimeThisYear()) 
-                     ->setStatus($this->faker->randomElement(['active', 'inactive', 'pending'])); 
+                ->setInfo($this->faker->sentence(10)) // Utilition de Faker via $this->faker
+                ->setCreatedAt($this->faker->dateTimeThisYear())
+                ->setUpdatedAt($this->faker->dateTimeThisYear())
+                ->setStatus($this->faker->randomElement(['on', 'off', 'pending']));
 
             $manager->persist($infoType);
             $this->addReference(self::PREFIX . $i, $infoType); // Ajout de la référence
