992773819bd067f169f35359f377f43c52a204d9
godefroyhanot <godefroy.hanot@hotmail.fr>
2024-10-18 12:20:06 +0200

    #10 - Soft Delete Crud Pour Les Contacts

--- src/DataFixtures/ContactFixtures.php
+++ src/DataFixtures/ContactFixtures.php
@@ -4,9 +4,10 @@ namespace App\DataFixtures;
 
 use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ContactFixtures extends Fixture
+class ContactFixtures extends Fixture implements DependentFixtureInterface
 {
     public const PREFIX = "contact#";
     public const POOL_MAX = 10;
@@ -14,14 +15,21 @@ class ContactFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        $prefixClient = ClientFixtures::PREFIX;
+        $clients = [];
+        for ($i = ClientFixtures::POOL_MIN; $i < ClientFixtures::POOL_MAX; $i++) {
+            $clients[] = $prefixClient . $i;
+        }
 
         $contactsData = [];
         for ($i = self::POOL_MIN; $i < self::POOL_MAX; $i++) {
+            $client = $this->getReference($clients[array_rand($clients, 1)]);
             $contact = new Contact();
             $contact
                 ->setName(self::PREFIX . $i)
                 ->setCreatedAt(new \DateTime())
                 ->setUpdatedAt(new \DateTime())
+                ->setClient($client)
                 ->setStatus('on');
             $manager->persist($contact);
             $this->addReference(self::PREFIX . $i, $contact);
@@ -29,5 +37,12 @@ class ContactFixtures extends Fixture
         }
 
         $manager->flush(); 
+    }
+
+    public function getDependencies()
+    {
+        return [
+            ClientFixtures::class,
+        ];
     }
 }
